eb7c6fd8aeaa2ac060a8922b410f72525b595786
bymontana <montanahayes@mail.com>
2025-08-08 17:29:27 -0400

    First step, create object

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 //Write your code here
+// Create a variable named attendee and assign it an object with the following properties:
+// attendeeID (string) with the value T001
+// name (a string) with the value Alice Smith
+// event (a string) with the value JavaScript Conference
+// ticketType (a streing) with the value VIP
+// ticketPrice (a number) with the value 150.00
+
+let attendee = {
+  attendeeID: 'T001',
+  name: 'Alice Smith',
+  event: 'JavaScript Conference',
+  ticketType: 'VIP',
+  ticketPrice: 150.00
+};
+
+
+
 
 
 
